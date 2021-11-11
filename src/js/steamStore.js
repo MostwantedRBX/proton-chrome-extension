6687f31bd8f1ee6c2e147c85ed2bde4a697f96db
MostwantedRBX <mostwantedrbxsteam@gmail.com>
2021-11-11 16:48:45 -0600

    refactoring and commenting

--- src/js/steamStore.js
+++ src/js/steamStore.js
@@ -4,33 +4,30 @@
 var id = window.location.href.slice(35).split("/")[0];
 var protonAppLink = "https://www.protondb.com/app/" + id;
 
-
-chrome.runtime.sendMessage(
-    {
+//  send app id to the listener in background.js > callback the createProtonButton with the results
+chrome.runtime.sendMessage({
         contentScriptQuery: "queryProtonRating",
-        appID: id
-    },
-    createProtonButton
-);
-
+        appID: id }, 
+        createProtonButton
+        );
 
 function createProtonButton(rating) {
+    //  Create a div.
 
     var cont = document.createElement("div");
     cont.className = "proton_rating_div proton_"+ rating;
 
+    //  Create an anchor link, set the href to the protondb page, add it to the container div.
     var pageLink = document.createElement("a");
     pageLink.className = "proton_rating_link";
     pageLink.href = protonAppLink;
     pageLink.text = "Proton: " + rating[0].toUpperCase()+rating.substring(1);
     pageLink.target = "_blank";
     cont.appendChild(pageLink);
-
+    
+    //  Get the "Community Hub" button on the steam page and append the new div to the parent of the button.
     var otherSiteButton = document.getElementsByClassName("apphub_OtherSiteInfo");
     if (otherSiteButton) {
         otherSiteButton[0].append(cont);
-    } else {
-        console.log("Cannot select other site button")
     }
-    
 }
