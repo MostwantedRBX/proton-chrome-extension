b9e5bc87b6128d9f6935a2d8b74ef2b28485d19b
MostwantedRBX <mostwantedrbxsteam@gmail.com>
2021-11-11 16:48:59 -0600

    refactoring and commenting

--- src/js/background.js
+++ src/js/background.js
@@ -3,27 +3,21 @@
 chrome.runtime.onMessage.addListener(
   function(req, from, sendRes) {
       if (req.contentScriptQuery == "queryProtonRating") {
+          //    link to game ratings on proton
           var href = "https://www.protondb.com/api/v1/reports/summaries/" + req.appID + ".json";
 
+          //    Fetch the body from the appid's protondb page > if response if 404, that means its not been reviewed enough > return the rating field in the json
           fetch(href)
-              .then(response => {
-                  if (!response.ok)
-                  {
-                      if (response.status == 404)
-                      {
+              .then(res => {
+                  if (!res.ok) {
+                      if (res.status == 404) {
                           sendRes("pending");
                       }
-
-                      throw Error(response.status);
+                      throw Error(res.status);
                   }
-                  return response.json();
+                  return res.json();
               })
-              .then(data => {
-                console.log(data.tier); 
-                sendRes(data.tier);
-
-              })
-              .catch(error => console.log(error))
+              .then(data => {sendRes(data.tier);}).catch(error => console.log(error))
 
           return true;
       }
